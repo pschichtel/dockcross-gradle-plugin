23e75f1072e0852de225cea20cbd5d6620a1247c
Phillip Schichtel <phillip@schich.tel>
2024-07-02 02:00:12 +0200

    add additional metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,16 @@ dependencies {
 }
 
 gradlePlugin {
+    displayName
     website = "https://github.com/pschichtel/dockcross-gradle-plugin"
     vcsUrl = "https://github.com/pschichtel/dockcross-gradle-plugin"
     plugins {
         create("dockcrossPlugin") {
             id = "tel.schich.dockcross"
             implementationClass = "tel.schich.dockcross.DockcrossPlugin"
+            displayName = "Dockcross Gradle Plugin"
+            description = "A simple gradle plugin to run tasks in dockcross containers"
+            tags = listOf("dockcross", "cross-compilation")
         }
     }
 }
